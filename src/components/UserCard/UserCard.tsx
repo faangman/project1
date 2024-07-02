c2963b85a952129a44a50c17878b9b868b2ee8f3
faangman <faang.net@gmail.com>
2024-07-01 19:25:09 -0700

    add behaviour frame 2

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,13 +1,13 @@
 import {UserType} from "../../UsersReducer";
 import s from './usercard.module.css'
-import defaultlogo from '../../assets/images/defaultlogo.jpeg'
+import defaultava from '../../assets/images/defaultlogo.jpeg'
 
 type Props = UserType
 export const UserCard = ({id, status, name, followed, photos}: Props) => {
 
     return (
         <div className={s.usercard}>
-            <img className={s.img} src={defaultlogo} alt="ava"/>
+            <img className={s.img} src={defaultava} alt="ava"/>
             <div>ID: {id}</div>
             <div>Name: {name}</div>
             <div>Status: {status}</div>
